50b6f19f130b6fbc6a3d8fdc14389bba3e908754
ken1flan <ken1flan@gmail.com>
2023-03-18 17:34:10 +0900

    Articleの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :articles
   root to: 'top#index'
 end
--- spec/requests/top_request_spec.rb
+++ spec/requests/top_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Tops", type: :request do
 
-  describe "GET /index" do
+  describe "GET /" do
     it "returns http success" do
-      get "/top/index"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
